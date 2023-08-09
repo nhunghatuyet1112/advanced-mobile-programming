8a5777187f9360d1065a8319128497a2bdddee07
Huy0811 <duongchihuy.study@gmail.com>
2023-08-09 16:19:28 +0700

    FigmaUpdate:"Login"

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:finalproject/utils.dart';
 import 'package:finalproject/pages/login.dart';
+// import 'package:finalproject/components/frame-1171274895.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(
+      home: Scaffold(
         body: SingleChildScrollView(
           child: Scene(),
         ),
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,191 +1,179 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:finalproject/utils.dart';
 
 class Scene extends StatelessWidget {
-  const Scene({super.key});
-
   @override
   Widget build(BuildContext context) {
     double baseWidth = 412;
     double fem = MediaQuery.of(context).size.width / baseWidth;
     double ffem = fem * 0.97;
-    TextEditingController usernameController = TextEditingController();
-    TextEditingController passwordController = TextEditingController();
-    return SizedBox(
+    return Container(
       width: double.infinity,
       child: Container(
-        padding: EdgeInsets.fromLTRB(35 * fem, 50 * fem, 35 * fem, 0 * fem),
+        // loginX9K (2488:1008)
+        padding: EdgeInsets.fromLTRB(31*fem, 39*fem, 31*fem, 29*fem),
         width: double.infinity,
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration (
           color: Color(0xffffffff),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
-              margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 0 * fem),
-              constraints: BoxConstraints(
-                maxWidth: 261 * fem,
+              // urbanoutfittersE3j (2488:1023)
+              margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 6*fem),
+              constraints: BoxConstraints (
+                maxWidth: 261*fem,
               ),
               child: Text(
                 'Urban Outfitters',
                 textAlign: TextAlign.center,
-                style: SafeGoogleFont(
+                style: SafeGoogleFont (
                   'Alfa Slab One',
-                  fontSize: 50 * ffem,
+                  fontSize: 50*ffem,
                   fontWeight: FontWeight.w400,
-                  height: 1.3675 * ffem / fem,
-                  color: const Color(0xff000000),
+                  height: 1.3675*ffem/fem,
+                  color: Color(0xff000000),
                 ),
               ),
             ),
             Container(
-              margin: EdgeInsets.fromLTRB(0 * fem, 30 * fem, 0 * fem, 30 * fem),
+              // loginhCD (2488:1022)
+              margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 50*fem),
               child: Text(
                 'Login',
                 textAlign: TextAlign.center,
-                style: SafeGoogleFont(
+                style: SafeGoogleFont (
                   'Be Vietnam',
-                  fontSize: 30 * ffem,
-                  fontWeight: FontWeight.w600,
-                  height: 1.2575 * ffem / fem,
-                  color: const Color(0xff000000),
+                  fontSize: 25*ffem,
+                  fontWeight: FontWeight.w400,
+                  height: 1.2575*ffem/fem,
+                  color: Color(0xff000000),
                 ),
               ),
             ),
             Container(
-              margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 50 * fem),
+              // loginformc4H (2488:1012)
+              margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 99*fem),
               width: double.infinity,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Container(
-                    margin: EdgeInsets.fromLTRB(
-                        0 * fem, 0 * fem, 0 * fem, 10 * fem),
+                    // usernamew6Z (2488:1019)
+                    margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 9*fem),
                     width: double.infinity,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Container(
-                          margin: EdgeInsets.fromLTRB(
-                              0 * fem, 0 * fem, 0 * fem, 5 * fem),
+                          // usernameUMP (2488:1020)
+                          margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 14*fem),
                           width: double.infinity,
                           child: Text(
                             'Username',
-                            textAlign: TextAlign.start,
-                            style: SafeGoogleFont(
+                            textAlign: TextAlign.center,
+                            style: SafeGoogleFont (
                               'Be Vietnam',
-                              fontSize: 20 * ffem,
+                              fontSize: 20*ffem,
                               fontWeight: FontWeight.w400,
-                              height: 1.2575 * ffem / fem,
-                              color: const Color(0xff000000),
+                              height: 1.2575*ffem/fem,
+                              color: Color(0xff000000),
                             ),
                           ),
                         ),
                         Container(
-                          padding: EdgeInsets.fromLTRB(
-                              10 * fem, 0 * fem, 0 * fem, 0 * fem),
+                          // frame5ncy (2488:1021)
                           width: double.infinity,
-                          height: 49 * fem,
-                          decoration: BoxDecoration(
-                            color: const Color(0xffededed),
-                            borderRadius: BorderRadius.circular(10 * fem),
-                          ),
-                          child: TextField(
-                            controller: usernameController,
-                            decoration:
-                                const InputDecoration(border: InputBorder.none),
-                            // onChanged: (e) => usernameController.text = e,
+                          height: 49*fem,
+                          decoration: BoxDecoration (
+                            color: Color(0xffededed),
+                            borderRadius: BorderRadius.circular(10*fem),
                           ),
                         ),
                       ],
                     ),
                   ),
                   Container(
-                    margin: EdgeInsets.fromLTRB(
-                        0 * fem, 0 * fem, 0 * fem, 10 * fem),
+                    // passwordLPb (2488:1016)
+                    margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 9*fem),
                     width: double.infinity,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Container(
-                          margin: EdgeInsets.fromLTRB(
-                              0 * fem, 0 * fem, 0 * fem, 5 * fem),
+                          // passwordSxR (2488:1017)
+                          margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 14*fem),
                           width: double.infinity,
                           child: Text(
                             'Password',
-                            textAlign: TextAlign.start,
-                            style: SafeGoogleFont(
+                            textAlign: TextAlign.center,
+                            style: SafeGoogleFont (
                               'Be Vietnam',
-                              fontSize: 20 * ffem,
+                              fontSize: 20*ffem,
                               fontWeight: FontWeight.w400,
-                              height: 1.2575 * ffem / fem,
-                              color: const Color(0xff000000),
+                              height: 1.2575*ffem/fem,
+                              color: Color(0xff000000),
                             ),
                           ),
                         ),
                         Container(
-                          padding: EdgeInsets.fromLTRB(
-                              10 * fem, 0 * fem, 0 * fem, 0 * fem),
+                          // frame5N5P (2488:1018)
                           width: double.infinity,
-                          height: 49 * fem,
-                          decoration: BoxDecoration(
-                            color: const Color(0xffededed),
-                            borderRadius: BorderRadius.circular(10 * fem),
-                          ),
-                          child: TextField(
-                            obscureText: true,
-                            enableSuggestions: false,
-                            autocorrect: false,
-                            controller: passwordController,
-                            decoration:
-                                const InputDecoration(border: InputBorder.none),
-                            // onChanged: (e) => usernameController.text = e,
+                          height: 49*fem,
+                          decoration: BoxDecoration (
+                            color: Color(0xffededed),
+                            borderRadius: BorderRadius.circular(10*fem),
                           ),
                         ),
                       ],
                     ),
                   ),
                   Container(
-                    margin: EdgeInsets.fromLTRB(
-                        0 * fem, 0 * fem, 0 * fem, 20 * fem),
+                    // forgotpasswordWSV (2488:1015)
+                    margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 20*fem),
                     width: double.infinity,
                     child: Text(
-                      'Forgot password ?',
+                      'Forgot password?',
                       textAlign: TextAlign.right,
-                      style: SafeGoogleFont(
+                      style: SafeGoogleFont (
                         'Be Vietnam',
-                        fontSize: 18 * ffem,
+                        fontSize: 18*ffem,
                         fontWeight: FontWeight.w400,
-                        height: 1.2575 * ffem / fem,
-                        color: const Color(0xff000000),
-                        decorationColor: const Color(0xff000000),
+                        height: 1.2575*ffem/fem,
+                        decoration: TextDecoration.underline,
+                        color: Color(0xff000000),
+                        decorationColor: Color(0xff000000),
                       ),
                     ),
                   ),
                   TextButton(
+                    // loginuUd (2488:1013)
                     onPressed: () {},
-                    style: TextButton.styleFrom(
+                    style: TextButton.styleFrom (
                       padding: EdgeInsets.zero,
                     ),
                     child: Container(
                       width: double.infinity,
-                      height: 40 * fem,
-                      decoration: BoxDecoration(
-                        color: const Color(0xff292526),
-                        borderRadius: BorderRadius.circular(40 * fem),
+                      height: 40*fem,
+                      decoration: BoxDecoration (
+                        color: Color(0xff292526),
+                        borderRadius: BorderRadius.circular(40*fem),
                       ),
                       child: Center(
                         child: Center(
                           child: Text(
                             'Login',
                             textAlign: TextAlign.center,
-                            style: SafeGoogleFont(
+                            style: SafeGoogleFont (
                               'Be Vietnam',
-                              fontSize: 20 * ffem,
+                              fontSize: 20*ffem,
                               fontWeight: FontWeight.w700,
-                              height: 1.2575 * ffem / fem,
-                              color: const Color(0xffffffff),
+                              height: 1.2575*ffem/fem,
+                              color: Color(0xffffffff),
                             ),
                           ),
                         ),
@@ -196,58 +184,60 @@ class Scene extends StatelessWidget {
               ),
             ),
             Container(
-              margin: EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 0 * fem),
+              // registerlinkAvM (2488:1009)
+              margin: EdgeInsets.fromLTRB(86*fem, 0*fem, 86*fem, 0*fem),
               width: double.infinity,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Container(
-                    margin:
-                        EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 0 * fem),
+                    // donthaveaccount6Z7 (2488:1010)
+                    margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 9*fem),
                     child: Text(
-                      'Don’t have account ?',
+                      'Don’t have account?',
                       textAlign: TextAlign.center,
-                      style: SafeGoogleFont(
+                      style: SafeGoogleFont (
                         'Be Vietnam',
-                        fontSize: 20 * ffem,
+                        fontSize: 20*ffem,
                         fontWeight: FontWeight.w700,
-                        height: 1.2575 * ffem / fem,
-                        color: const Color(0xff000000),
+                        height: 1.2575*ffem/fem,
+                        color: Color(0xff000000),
                       ),
                     ),
                   ),
                   TextButton(
+                    // linkheretoregisteroCd (2488:1011)
                     onPressed: () {},
-                    style: TextButton.styleFrom(
+                    style: TextButton.styleFrom (
                       padding: EdgeInsets.zero,
                     ),
                     child: RichText(
                       textAlign: TextAlign.center,
                       text: TextSpan(
-                        style: SafeGoogleFont(
+                        style: SafeGoogleFont (
                           'Be Vietnam',
-                          fontSize: 20 * ffem,
+                          fontSize: 20*ffem,
                           fontWeight: FontWeight.w400,
-                          height: 1.2575 * ffem / fem,
-                          color: const Color(0xff000000),
+                          height: 1.2575*ffem/fem,
+                          color: Color(0xff000000),
                         ),
                         children: [
-                          const TextSpan(
-                            text: 'Click ',
+                          TextSpan(
+                            text: 'Link ',
                           ),
                           TextSpan(
                             text: 'here',
-                            style: SafeGoogleFont(
+                            style: SafeGoogleFont (
                               'Be Vietnam',
-                              fontSize: 20 * ffem,
+                              fontSize: 20*ffem,
                               fontWeight: FontWeight.w700,
-                              height: 1.2575 * ffem / fem,
+                              height: 1.2575*ffem/fem,
                               decoration: TextDecoration.underline,
-                              color: const Color(0xff00a3ff),
-                              decorationColor: const Color(0xff00a3ff),
+                              color: Color(0xff00a3ff),
+                              decorationColor: Color(0xff00a3ff),
                             ),
                           ),
-                          const TextSpan(
+                          TextSpan(
                             text: ' to register',
                           ),
                         ],
@@ -260,6 +250,6 @@ class Scene extends StatelessWidget {
           ],
         ),
       ),
-    );
+          );
   }
 }
